c5326866970e59c5d1a18c5a29fa021123472298
elbasel42 <150071951+elbasel42@users.noreply.github.com>
2023-12-29 21:08:59 +0000

    Added proxy

--- app/components/AppPlayer.tsx
+++ app/components/AppPlayer.tsx
@@ -1,17 +1,55 @@
 "use client";
-import { Player, Hls } from "@vime/react";
+
+import { createRef, useRef } from "react";
+import ReactPlayer from "react-player";
+
+// import { Player, Hls, DefaultUi } from "@vime/react";
 
 interface Props {
   url: string;
+  subtitleUrl?: string;
+  onEnded?: Function;
 }
-export const AppPlayer = ({ url }: Props) => {
+export const AppPlayer = ({ url, subtitleUrl }: Props) => {
+  const playerRef: any = useRef();
   return (
-    <div>
-      <Player controls>
-        <Hls crossOrigin="anonymous">
-          <source data-src={url} type="application/x-mpegURL" />
-        </Hls>
-      </Player>
-    </div>
-  );
+    <ReactPlayer url={url} ref={playerRef} controls 
+    config={{
+      file: {
+        attributes: {
+
+          crossOrigin: "anonymous"
+        },
+        tracks: [
+          {kind: "subtitles", src: subtitleUrl, srcLang: "en", default: true, label: "English"}
+        ]
+      }
+    }}
+    onReady={() => {
+      const player: any = playerRef.current.getInternalPlayer('hls');
+      console.log(player.levels)
+      player.currentLevel = 0;
+      setTimeout(() => {
+        console.log({currentLevel: player.currentLevel})
+      }, 1000); 
+    }}/>
+  )
+//   return (
+//     <div className="w-1/2">
+//       <Player>
+//         <Hls crossOrigin="anonymous">
+//           <source data-src={url} type="application/x-mpegURL" />
+//         </Hls>
+//         <DefaultUi />
+//         <track
+//           default
+//           kind="subtitles"
+//           // src={subtitleUrl ?? ""}
+//           src="https://ccb.megaresources.co/c9/6d/c96d8ba5cea6c3ccd7d76c36f0fa073d/c96d8ba5cea6c3ccd7d76c36f0fa073d.vtt"
+//           srcLang="en"
+//           label="English"
+//         />
+//       </Player>
+//     </div>
+//   );
 };
--- app/watch/[id]/[episodeId]/page.tsx
+++ app/watch/[id]/[episodeId]/page.tsx
@@ -1,24 +1,29 @@
 import { AppPlayer } from "@/app/components/AppPlayer";
-import "@vime/core/themes/default.css";
 import { AnimeProvider } from "@/app/lib/AnimeProvider";
+import "@vime/core/themes/default.css";
 
 const EpisodePage = async ({ params }: { params: { episodeId: string } }) => {
   const { episodeId } = params;
   const validEpisodeId = episodeId.replaceAll("%24", "$");
-  const { sources } = await AnimeProvider.fetchEpisodeSources(validEpisodeId);
+  const { sources, intro, outro, subtitles, download, embedURL } = await AnimeProvider.fetchEpisodeSources(validEpisodeId);
+  console.log({sources, intro, outro, subtitles, download })
+  const englishSubtitle = subtitles?.at(0)
+  
 
   return (
     <>
       <h1>{validEpisodeId}</h1>
       <div>
-        {sources.map(({ url, isM3U8, isDASH }) => {
+        {sources.map(({ url, isM3U8, isDASH, quality, size }) => {
+          const proxyUrl = `https://m3u8-proxy-cors-hazel.vercel.app/cors?url=${url}`
+          console.log({quality, size})
           return (
             <div className="border border-black">
               <h2>URL: {url}</h2>
               <p>isM3U8: {isM3U8 ? "true" : "false"}</p>
               <p>isDASH: {isDASH ? "true" : "false"}</p>
-              <div>
-                <AppPlayer url={url}/>
+              <div className="flex justify-center">
+                <AppPlayer url={proxyUrl} subtitleUrl={englishSubtitle?.url}/> 
               </div>
             </div>
           );
