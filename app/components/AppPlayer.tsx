6fd3863c8aeec6044f225e00e3b7526296daf58c
elbasel42 <150071951+elbasel42@users.noreply.github.com>
2023-12-30 09:44:16 +0000

    Subtitles working

--- app/components/AppPlayer.tsx
+++ app/components/AppPlayer.tsx
@@ -1,53 +1,78 @@
 "use client";
 
-import {  useRef } from "react";
+import { useEffect, useRef } from "react";
 import ReactPlayer from "react-player";
-
 
 interface Props {
   url: string;
-  subtitleUrl?: string;
+  subtitles?: any;
 }
-export const AppPlayer = ({ url, subtitleUrl = ""}: Props) => {
+export const AppPlayer = ({ url, subtitles }: Props) => {
   const playerRef: any = useRef();
-  return (
-    <ReactPlayer url={url} ref={playerRef} controls 
-    config={{
-      file: {
-        attributes: {
 
-          crossOrigin: "anonymous"
+  useEffect(() => {}, []);
+  return (
+    <ReactPlayer
+      url={url}
+      ref={playerRef}
+      controls
+      config={{
+        file: {
+          attributes: {
+            crossOrigin: "anonymous",
+          },
+          tracks: [
+            // {kind: "subtitles", src: subtitleUrl ?? "", srcLang: "en", default: true, label: "English"}
+            ...subtitles.map((s: { url: string; lang: string }) => {
+              let def = false;
+              let srcLang = "";
+              if (s.lang === "Thumbnails") return;
+              if (["en", "english"].includes(s.lang.toLowerCase())) {
+                srcLang = "en";
+              }
+
+              return {
+                kind: "subtitles",
+                src: s.url,
+                label: s.lang,
+                srclang: srcLang,
+              };
+            }),
+          ],
         },
-        tracks: [
-          {kind: "subtitles", src: subtitleUrl, srcLang: "en", default: true, label: "English"}
-        ]
-      }
-    }}
-    onReady={() => {
-      const player: any = playerRef.current.getInternalPlayer('hls');
-      console.log(player.levels)
-      player.currentLevel = 0;
-      setTimeout(() => {
-        console.log({currentLevel: player.currentLevel})
-      }, 1000); 
-    }}/>
-  )
-//   return (
-//     <div className="w-1/2">
-//       <Player>
-//         <Hls crossOrigin="anonymous">
-//           <source data-src={url} type="application/x-mpegURL" />
-//         </Hls>
-//         <DefaultUi />
-//         <track
-//           default
-//           kind="subtitles"
-//           // src={subtitleUrl ?? ""}
-//           src="https://ccb.megaresources.co/c9/6d/c96d8ba5cea6c3ccd7d76c36f0fa073d/c96d8ba5cea6c3ccd7d76c36f0fa073d.vtt"
-//           srcLang="en"
-//           label="English"
-//         />
-//       </Player>
-//     </div>
-//   );
+      }}
+      onReady={() => {
+        const player: any = playerRef.current.getInternalPlayer("hls");
+        console.log(player.levels);
+        player.currentLevel = 0;
+        setTimeout(() => {
+          console.log({ currentLevel: player.currentLevel });
+        }, 1000);
+
+        const playerElem = document.querySelector("video");
+        const textTracks = playerElem?.textTracks ?? [];
+        textTracks[0].mode = "showing";
+        console.log({ playerElem });
+        console.log({ textTracks });
+      }}
+    />
+  );
+  //   return (
+  //     <div className="w-1/2">
+  //       <Player>
+  //         <Hls crossOrigin="anonymous">
+  //           <source data-src={url} type="application/x-mpegURL" />
+  //         </Hls>
+  //         <DefaultUi />
+  //         <track
+  //           default
+  //           kind="subtitles"
+  //           // src={subtitleUrl ?? ""}
+  //           src="https://ccb.megaresources.co/c9/6d/c96d8ba5cea6c3ccd7d76c36f0fa073d/c96d8ba5cea6c3ccd7d76c36f0fa073d.vtt"
+  //           srcLang="en"
+  //           label="English"
+  //         />
+  //       </Player>
+  //     </div>
+  //   );
 };
--- app/watch/[id]/[episodeId]/page.tsx
+++ app/watch/[id]/[episodeId]/page.tsx
@@ -1,28 +1,27 @@
 import { AppPlayer } from "@/app/components/AppPlayer";
 import { AnimeProvider } from "@/app/lib/AnimeProvider";
 
-const EpisodePage = async ({ params }: { params: { episodeId: string} }) => {
+const EpisodePage = async ({ params }: { params: { episodeId: string } }) => {
   const { episodeId } = params;
   const validEpisodeId = episodeId.replaceAll("%24", "$");
-  const { sources, intro, outro, subtitles, download, embedURL } = await AnimeProvider.fetchEpisodeSources(validEpisodeId);
-  console.log({sources, intro, outro, subtitles, download })
-  const englishSubtitle = subtitles?.at(0)
-  
+  const { sources, intro, outro, subtitles, download, embedURL } =
+    await AnimeProvider.fetchEpisodeSources(validEpisodeId);
+  console.log({ sources, intro, outro, subtitles, download });
 
   return (
     <>
       <h1>{validEpisodeId}</h1>
       <div>
         {sources.map(({ url, isM3U8, isDASH, quality, size }) => {
-          const proxyUrl = `https://m3u8-proxy-cors-hazel.vercel.app/cors?url=${url}`
-          console.log({quality, size})
+          const proxyUrl = `https://m3u8-proxy-cors-hazel.vercel.app/cors?url=${url}`;
+          console.log({ quality, size });
           return (
             <div className="border border-black">
               <h2>URL: {url}</h2>
               <p>isM3U8: {isM3U8 ? "true" : "false"}</p>
               <p>isDASH: {isDASH ? "true" : "false"}</p>
               <div className="flex justify-center">
-                <AppPlayer url={proxyUrl} subtitleUrl={englishSubtitle?.url}/> 
+              <AppPlayer url={proxyUrl} subtitles={subtitles}/>
               </div>
             </div>
           );
