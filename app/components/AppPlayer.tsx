60f192946a53cbdab59de73665839e65286aaf86
elbasel42 <150071951+elbasel42@users.noreply.github.com>
2024-01-06 18:35:45 +0000

    Add subtitle selection

--- app/components/AppPlayer.tsx
+++ app/components/AppPlayer.tsx
@@ -11,12 +11,29 @@ export const AppPlayer = ({ url, subtitles }: Props) => {
   const playerRef: any = useRef();
   const [qualities, setQualities] = useState([]);
   const [currentQuality, setCurrentQuality] = useState("");
+  const [currentSubtitle, setCurrentSubtitle] = useState("");
 
   useEffect(() => {
     if (qualities.length === 0) return;
     const savedQualityIndex = localStorage.getItem("savedQualityIndex") ?? 0;
     setCurrentQuality(qualities[+savedQualityIndex]);
   }, [qualities]);
+
+  const onSubtitleChange = (e: any) => {
+    const selectedSubtitle = e.target.value;
+    setCurrentSubtitle(selectedSubtitle);
+    localStorage.setItem("savedSubtitleLang", selectedSubtitle);
+    console.log({ selectedSubtitle });
+    const playerElem = document.querySelector("video");
+    const textTracks: any = playerElem?.textTracks ?? [];
+    [...textTracks].forEach((t) => {
+      const trackLabel = t.label;
+      t.mode = "hidden";
+      if (trackLabel === selectedSubtitle) {
+        t.mode = "showing";
+      }
+    });
+  };
 
   const onQualityChange = (e: any) => {
     const selectedQuality = e.target.value;
@@ -49,11 +66,23 @@ export const AppPlayer = ({ url, subtitles }: Props) => {
     }, 2000);
 
     //! Set subtitle
+    const savedSubtitleLang = localStorage.getItem("savedSubtitleLang");
+    setCurrentSubtitle(savedSubtitleLang ?? "");
+    console.log({ savedSubtitleLang });
+
     const playerElem = document.querySelector("video");
-    const textTracks = playerElem?.textTracks ?? [];
-    textTracks[0].mode = "showing";
-    console.log({ playerElem });
-    console.log({ textTracks });
+    const textTracks: any = playerElem?.textTracks ?? [];
+    [...textTracks].forEach((t) => {
+      const trackLabel = t.label;
+      console.log({ trackLabel });
+      console.log(trackLabel === savedSubtitleLang);
+      t.mode = "hidden";
+      if (trackLabel === savedSubtitleLang) t.mode = "showing";
+    });
+
+    // textTracks[0].mode = "showing";
+    // console.log({ playerElem });
+    // console.log({ textTracks });
   };
 
   return (
@@ -86,17 +115,31 @@ export const AppPlayer = ({ url, subtitles }: Props) => {
         }}
         onReady={onPlayerReady}
       />
-      <select
-        onChange={onQualityChange}
-        name="quality"
-        id="quality"
-        className="bg-black rounded-3xl px-4 py-2"
-        value={currentQuality}
-      >
-        {qualities.map((q) => {
-          return <option value={q}>{q}p</option>;
-        })}
-      </select>
+      <div className="mt-4 flex gap-4">
+        <select
+          onChange={onQualityChange}
+          name="quality"
+          id="quality"
+          className="bg-black rounded-3xl px-4 py-2"
+          value={currentQuality}
+        >
+          {qualities.map((q) => {
+            return <option value={q}>{q}p</option>;
+          })}
+        </select>
+        <select
+          value={currentSubtitle}
+          name="subtitle"
+          id="subtitle"
+          className="bg-black rounded-3xl px-4 py-2"
+          onChange={onSubtitleChange}
+        >
+          {subtitles.map(({ lang }: { lang: string }) => {
+            if (lang === "Thumbnails") return;
+            return <option value={lang}>{lang}</option>;
+          })}
+        </select>
+      </div>
     </div>
   );
 };
